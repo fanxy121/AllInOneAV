bae92061048df12e5ccff2d1ee24807f446404b3
Shuang Qiu <cleuswitz@outlook.com>
2018-05-24 10:37:01 +0800

    add unitest

--- AllInOneAV/UnitTest/Program.cs
+++ AllInOneAV/UnitTest/Program.cs
@@ -1,6 +1,9 @@
 ﻿using Model.Common;
+using Model.ScanModels;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,7 +14,16 @@ namespace UnitTest
     {
         static void Main(string[] args)
         {
-            
+            StreamReader sr = new StreamReader("c:\\AvLogScanJson2018-05-24-10-31-22.json", Encoding.UTF8);
+            var str = sr.ReadLine();
+            sr.Close();
+
+            Console.WriteLine(str);
+            Console.ReadKey();
+
+            var list = JsonConvert.DeserializeObject<List<DuplicateItem>>(str);
+
+            Console.ReadKey();
         }
     }
 }
