6700a3af56ef473a58d01527f15547c4d29668e2
Shuang Qiu <cleuswitz@outlook.com>
2020-01-13 10:56:31 +0800

    update

---
+++ AllInOneAV/JavBusDownload/JavBusDownloadHelper.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JavBusDownload
+{
+    public class JavBusDownloadHelper
+    {
+        public static List<string> GetCategory()
+        {
+            List<string> ret = new List<string>();
+            var index = "https://www.javbus.com";
+            var url = "https://www.javbus.com/genre";
+
+
+            return ret;
+        }
+    }
+}
--- AllInOneAV/JavBusDownload/Program.cs
+++ AllInOneAV/JavBusDownload/Program.cs
@@ -10,6 +10,7 @@ namespace JavBusDownload
     {
         static void Main(string[] args)
         {
+
         }
     }
 }
